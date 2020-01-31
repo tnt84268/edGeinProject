20e692e25015a8a896bc36d44b25f8c52f018eea
tnt84268 <dannynguyen84268@gmail.com>
2020-01-31 21:24:08 +0000

    Test javascript

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+document.getElementById("msg").addEventListener("click", buttonPress);
 
+function buttonPress() {
+  document.getElementById("msg").innerHTML = "YOU CLICKED ME!";
+  Cookies.set("bool", "true");
+  console.log(Cookies.get("bool"));
+}
